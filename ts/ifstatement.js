0f623f5d3c4637fa3cca03d1a3199efc4ce3b97c
JAVA _TRAINING_ASSIGNMENTS <101304971+ShantiSV@users.noreply.github.com>
2022-03-23 10:45:43 +0530

    Add files via upload

---
+++ ts/ifstatement.js
@@ -0,0 +1,15 @@
+<script>  
+var a=50;  
+if(a==55){  
+document.write("a is equal to 55");  
+}  
+else if(a==45){  
+document.write("a is equal to 45");  
+}  
+else if(a==50){  
+document.write("a is equal to 50");  
+}  
+else{  
+document.write("a is not equal to 55, 45 or 50");  
+}  
+</script>  
